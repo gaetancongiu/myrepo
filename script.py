f09eb3251380d46329ad9b2d2ca0cdcb7e0626f6
CONGIU Gaétan <congiu.gaetan@univ-catholyon.fr>
2020-02-18 14:21:42 +0100

    Message indiquant le language de programmation utilisé

--- script.py
+++ script.py
@@ -3,5 +3,7 @@ i = 0
 a = 0
 while i < 5:
     a = a+5
+    print(a)
     i += 1
-print("ce programme permet de voir les multtiples de 5")
+print("ce programme permet de voir les multiples de 5")
+print("Le language utilisé est un language de programmation nommée python")
