35593f10820be66e33d8481d4e9e42b2bba726b5
CONGIU Gaétan <congiu.gaetan@univ-catholyon.fr>
2020-02-18 15:36:00 +0100

    Messsage Jeux des réponses

--- script.py
+++ script.py
@@ -7,3 +7,4 @@ while i < 5:
     i += 1
 print("ce programme permet de voir les multiples de 5")
 print("Le language utilisé est un language de programmation nommée python")
+print("Jeux des réponses")
